cc0e44aa503ac4c7d5cf963e06f4f5d5eeef05ec
rmaliyan <rmaliyan14@gmail.com>
2024-10-06 23:16:22 +0400

    added draft version of main website element

---
+++ src/App.jsx
@@ -0,0 +1,118 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+
+// function App() {
+//   const [count, setCount] = useState(0)
+
+//   return (
+//     <>
+//       <div>
+//         <a href="https://vitejs.dev" target="_blank">
+//           <img src={viteLogo} className="logo" alt="Vite logo" />
+//         </a>
+//         <a href="https://react.dev" target="_blank">
+//           <img src={reactLogo} className="logo react" alt="React logo" />
+//         </a>
+//       </div>
+//       <h1>Vite + React</h1>
+//       <div className="card">
+//         <button onClick={() => setCount((count) => count + 1)}>
+//           count is {count}
+//         </button>
+//         <p>
+//           Edit <code>src/App.jsx</code> and save to test HMR
+//         </p>
+//       </div>
+//       <p className="read-the-docs">
+//         Click on the Vite and React logos to learn more
+//       </p>
+//     </>
+//   )
+// }
+
+// export default App
+
+// https://cydstumpel.nl/
+
+import * as THREE from 'three'
+import { useRef, useState } from 'react'
+import { Canvas, useFrame } from '@react-three/fiber'
+import { Image, Environment, ScrollControls, useScroll, useTexture } from '@react-three/drei'
+import { easing } from 'maath'
+import './util'
+
+const App = () => (
+  <Canvas camera={{ position: [0, 0, 100], fov: 15 }}>
+    <color attach="background" args={['#15151a']} />
+    {/* <hemisphereLight intensity={0.5} /> */}
+    <fog attach="fog" args={['#15151a', 8.5, 12]} />
+    <ScrollControls pages={4} infinite>
+      <Rig rotation={[0, 0, 0.15]}>
+        <Carousel />
+      </Rig>
+      <Banner position={[0, -0.15, 0]} />
+    </ScrollControls>
+    {/* <Environment preset="city" background blur={0.5} /> */}
+  </Canvas>
+)
+
+function Rig(props) {
+  const ref = useRef()
+  const scroll = useScroll()
+  useFrame((state, delta) => {
+    ref.current.rotation.y = -scroll.offset * (Math.PI * 2) // Rotate contents
+    state.events.update() // Raycasts every frame rather than on pointer-move
+    easing.damp3(state.camera.position, [-state.pointer.x * 2, state.pointer.y + 1.5, 10], 0.3, delta) // Move camera
+    state.camera.lookAt(0, 0, 0) // Look at center
+  })
+  return <group ref={ref} {...props} />
+}
+
+function Carousel({ radius = 1.4, count = 2 }) {
+  return Array.from({ length: count }, (_, i) => (
+    <Card
+      key={i}
+      url={`/img${Math.floor(i % 10) + 1}_.jpg`}
+      position={[Math.sin((i / count) * Math.PI * 2) * radius, 0, Math.cos((i / count) * Math.PI * 2) * radius]}
+      rotation={[0, Math.PI + (i / count) * Math.PI * 2, 0]}
+    />
+  ))
+}
+
+function Card({ url, ...props }) {
+  const ref = useRef()
+  const [hovered, hover] = useState(false)
+  const pointerOver = (e) => (e.stopPropagation(), hover(true))
+  const pointerOut = () => hover(false)
+  useFrame((state, delta) => {
+    easing.damp3(ref.current.scale, hovered ? 1.15 : 1, 0.1, delta)
+    easing.damp(ref.current.material, 'radius', hovered ? 0.25 : 0.1, 0.2, delta)
+    easing.damp(ref.current.material, 'zoom', hovered ? 1 : 1.5, 0.2, delta)
+  })
+  return (
+    <Image ref={ref} url={url} transparent side={THREE.DoubleSide} onPointerOver={pointerOver} onPointerOut={pointerOut} {...props}>
+      <bentPlaneGeometry args={[0.1, 1, 1, 20, 20]} />
+    </Image>
+  )
+}
+
+function Banner(props) {
+  const ref = useRef()
+  const texture = useTexture('/work_.png')
+  texture.wrapS = texture.wrapT = THREE.RepeatWrapping
+  const scroll = useScroll()
+  useFrame((state, delta) => {
+    ref.current.material.time.value += Math.abs(scroll.delta) * 4
+    ref.current.material.map.offset.x += delta / 2
+  })
+  return (
+    <mesh ref={ref} {...props}>
+      <cylinderGeometry args={[1.6, 1.6, 0.14, 128, 16, true]} />
+      <meshSineMaterial map={texture} map-anisotropy={16} map-repeat={[30, 1]} side={THREE.DoubleSide} toneMapped={false} />
+    </mesh>
+  )
+}
+
+export default App;
